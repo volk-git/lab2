5b75010388277c7894129752a84478c1e05a6897
Anton <136102113+volk-git@users.noreply.github.com>
2023-12-18 21:47:11 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        MainFrame calc = new MainFrame();
-        calc.setDefaultCloseOperation(MainFrame.EXIT_ON_CLOSE);
-        calc.setVisible(true);
-
-    }
-}
