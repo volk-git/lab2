a357382b41df157e51e6615de1efd88864171f90
Anton <136102113+volk-git@users.noreply.github.com>
2023-12-18 21:48:18 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MainFrame calc = new MainFrame();
+        calc.setDefaultCloseOperation(MainFrame.EXIT_ON_CLOSE);
+        calc.setVisible(true);
+
+    }
+}
